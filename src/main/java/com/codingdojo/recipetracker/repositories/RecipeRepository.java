257ca098eeabb66f8a50f752ee1f25730c48a99d
scastro12 <sambigboy@aol.com>
2021-08-25 15:58:06 -0400

    added ingredients

--- src/main/java/com/codingdojo/recipetracker/repositories/RecipeRepository.java
+++ src/main/java/com/codingdojo/recipetracker/repositories/RecipeRepository.java
@@ -1,9 +1,12 @@
 package com.codingdojo.recipetracker.repositories;
 
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-import com.codingdojo.recipetracker.models.Recipe;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codingdojo.recipetracker.models.Recipe;
+@Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long>{
 	
 	List<Recipe> findAll();
