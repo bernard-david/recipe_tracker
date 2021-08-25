9d7a956462bf6c8983ed7f12ad646d4e0faeca97
scastro12 <sambigboy@aol.com>
2021-08-25 15:08:10 -0400

    populated homecontroller

--- src/main/java/com/codingdojo/recipetracker/controllers/HomeController.java
+++ src/main/java/com/codingdojo/recipetracker/controllers/HomeController.java
@@ -9,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.codingdojo.recipetracker.models.LoginUser;
+import com.codingdojo.recipetracker.models.Recipe;
 import com.codingdojo.recipetracker.models.User;
+import com.codingdojo.recipetracker.services.RecipeService;
 import com.codingdojo.recipetracker.services.UserService;
 
 @Controller
@@ -21,6 +24,8 @@ public class HomeController {
     
     @Autowired
     private UserService userServ;
+    @Autowired
+    private RecipeService recipeServ;
     
     public Long userSessionId(HttpSession session) {
     	if(session.getAttribute("user_id") == null)
@@ -28,6 +33,10 @@ public class HomeController {
     	return (Long)session.getAttribute("user_id");
     }
     
+    
+//    ------------------------------------------------------------------------------
+//    Main Home Page
+//    ------------------------------------------------------------------------------
     
     @GetMapping("/home")
     public String homePage(Model model, HttpSession session) {
@@ -37,7 +46,66 @@ public class HomeController {
     	}
         return "/home/index.jsp";
     }
+//    ------------------------------------------------------------------------------
     
+    
+//    ------------------------------------------------------------------------------
+//    New Recipe Page
+//    ------------------------------------------------------------------------------
+    
+    @GetMapping("/new-recipe")
+    public String newRecipePage(Model model, HttpSession session, @ModelAttribute("newRecipe") Recipe newRecipe) {
+    	Long userId = this.userSessionId(session);
+    	if(userId == null) {
+    		return "redirect:/";
+    	}
+        return "/recipes/newrecipe.jsp";
+    }
+    
+    @PostMapping("/recipes/create")
+    public String createRecipe(@Valid @ModelAttribute("newRecipe") Recipe newRecipe, 
+            BindingResult result, Model model, HttpSession session) {
+        if(result.hasErrors()) {
+            return "/recipes/newrecipe.jsp";
+        }
+        
+        return "redirect:/new-recipe";
+    }
+    
+    
+//    ------------------------------------------------------------------------------
+    
+    
+//    ------------------------------------------------------------------------------
+//    Show Recipe
+//    ------------------------------------------------------------------------------
+    
+    @GetMapping("/recipe/{id}")
+    public String newRecipePage(@PathVariable("id") Long id,Model model, HttpSession session) {
+    	Long userId = this.userSessionId(session);
+    	if(userId == null) {
+    		return "redirect:/";
+    	}
+    	model.addAttribute("recipe", recipeServ.getRecipe(id));
+        return "/recipes/showrecipe.jsp";
+    }
+    
+//    ------------------------------------------------------------------------------
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    ------------------------------------------------------------------------------
+//    Login Page
+//    ------------------------------------------------------------------------------
     
     @GetMapping("/")
     public String loginPage(Model model) {
@@ -45,7 +113,23 @@ public class HomeController {
         return "index.jsp";
     }
     
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+            BindingResult result, Model model, HttpSession session) {
+        User user = userServ.login(newLogin, result);
+        if(result.hasErrors()) {
+            model.addAttribute("newUser", new User());
+            return "index.jsp";
+        }
+        session.setAttribute("user_id", user.getId());
+        return "redirect:/home";
+    }
+//    ------------------------------------------------------------------------------
     
+    
+//    ------------------------------------------------------------------------------
+//    Registration
+//    ------------------------------------------------------------------------------
     
     @GetMapping("/registerUser")
     public String registerPage(Model model) {
@@ -66,17 +150,9 @@ public class HomeController {
         return "redirect:/home";
     }
     
-    @PostMapping("/login")
-    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
-            BindingResult result, Model model, HttpSession session) {
-        User user = userServ.login(newLogin, result);
-        if(result.hasErrors()) {
-            model.addAttribute("newUser", new User());
-            return "index.jsp";
-        }
-        session.setAttribute("user_id", user.getId());
-        return "redirect:/home";
-    }
+//    ------------------------------------------------------------------------------
+//    Logout
+//    ------------------------------------------------------------------------------
     
     @RequestMapping("/logout")
     public String logout(HttpSession session) {
@@ -85,3 +161,5 @@ public class HomeController {
     }
     
 }
+
+//	  ------------------------------------------------------------------------------
